f39b0d6d086fe6f34de19fe8191bc58044c2c21d
Marcel <marcel.kaempchen@gmail.com>
2025-08-13 11:20:03 +0000

    Second model is now trained correctly.

--- train.sh
+++ train.sh
@@ -1,27 +1,16 @@
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=6
 
 SCENE_PATH="scenes/marcus_light_sphere_hair.xml"
-OUTPUT_PATH="output/0812_train_tandem"
+OUTPUT_PATH="output/0812_train_tandem_2"
 
 SCENE_LIST=(
-	# unpolarized
+	unpolarized
 	indirect
 	direct
 )
 
 # python src/synthetic_gaussians/generate_images.py -s $SCENE_PATH -o $OUTPUT_PATH --res 1024 --spp 128
 # python src/synthetic_gaussians/pre_3dgs.py -w $OUTPUT_PATH
-
-# for SCENE in "${SCENE_LIST[@]}"; do
-# 	echo "gaussian reconstruction for: $SCENE"
-
-# 	# Train model
-# 	python submodules/gaussian-splatting/train.py -s $OUTPUT_PATH/$SCENE/colmap -m $OUTPUT_PATH/$SCENE/model --disable_viewer --eval
-
-# 	# Render results (test views + 360 video)
-# 	python submodules/gaussian-splatting/render.py -m $OUTPUT_PATH/$SCENE/model --output $OUTPUT_PATH/results/$SCENE --skip_train
-# 	python submodules/gaussian-splatting/render360.py -m $OUTPUT_PATH/$SCENE/model --output $OUTPUT_PATH/results/videos/$SCENE.mp4
-# done
 
 # Train unpolarized model
 # python submodules/gaussian-splatting/train.py -s $OUTPUT_PATH/unpolarized/colmap -m $OUTPUT_PATH/unpolarized/model --disable_viewer --eval
